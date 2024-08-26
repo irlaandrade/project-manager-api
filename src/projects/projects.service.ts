e4c8775e0be657013ce68e8e65b8e50ec01a6b46
irlaandrade <paula.irla@gmail.com>
2024-08-26 19:25:23 -0300

    feat - exercicio 3

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './entities/projects.entity';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create-project.dto';
+import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+
+  create(createProjectDto: CreateProjectDto) {
+    return this.projectRepository.save(createProjectDto);
+  }
+
+  findAll() {
+    return this.projectRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.projectRepository.findOne({
+      where: { id },
+      relations: {
+        tasks: true,
+      },
+    });
+  }
+
+  findOneByOrFail(id: number) {
+    return this.projectRepository.findOneByOrFail({ id });
+  }
+
+  update(id: number, updateProjectDto: UpdateProjectDto) {
+    return this.projectRepository.update(id, updateProjectDto);
+  }
+
+  remove(id: number) {
+    return this.projectRepository.delete(id); //Delete físico
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task]), ProjectsModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
